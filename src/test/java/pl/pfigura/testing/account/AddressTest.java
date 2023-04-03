dd47e41013961fc61f1a85d912a0b2ba2c940e3c
pfigura <piotr.figura.programista@gmail.com>
2023-04-03 16:44:06 +0200

    unit tests2

--- src/test/java/pl/pfigura/testing/account/AddressTest.java
+++ src/test/java/pl/pfigura/testing/account/AddressTest.java
@@ -1,7 +1,6 @@
 package pl.pfigura.testing.account;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
